38fdfb8acd3378017e7b53a878ce89e3e4fd95a3
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-15 17:43:55 +0530

    Add drivers/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <linux/of.h>
@@ -848,6 +849,7 @@ static void gsi_handle_irq(void)
 
 	if (type & GSI_EE_n_CNTXT_TYPE_IRQ_GENERAL_BMSK)
 		gsi_handle_general(ee);
+
 }
 
 static irqreturn_t gsi_isr(int irq, void *ctxt)
